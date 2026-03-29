58e6098de5c54b33e41295d3aba0d05e0e0dd330
caetanoag <2025311434@aluno.riogrande.ifrs.edu.br>
2026-03-29 14:20:08 -0300

    Aumentei o alfabeto

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ const dialogebox = document.getElementById("found-dialoge-box");
 const passwordForm = document.getElementById("password-form");
 const passwordBtn = document.getElementById("password-submit-btn");
 const alfabeto = {
-    'a': 0, 'b': 1,  'c': 2,
-    'd': 3, 'e': 4,  'f': 5,
-    'g': 6, 'h': 7,  'i': 8,
-    'j': 9, 'k': 10, 'l': 11,
-    'm': 12,'n': 13, 'o': 14,
-    'p': 15,'q': 16, 'r': 17,
-    's': 18,'t': 19, 'u': 20,
-    'v': 21,'w': 22, 'x': 23,
-    'y': 24,'z': 25,
+    'a': 0,  'b': 1,  'c': 2,  'd': 3,  'e': 4,
+    'f': 5,  'g': 6,  'h': 7,  'i': 8,  'j': 9,
+    'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14,
+    'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19,
+    'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,
+    'z': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29,
+    'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34,
+    'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39,
+    'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44,
+    'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49,
+    'Y': 50, 'Z': 51, '!': 52, '@': 53, '#': 54,
+    '$': 55, '%': 56, '&': 57, '*': 58, '(': 59,
+    ')': 60,
 }
 let tempo1 = Date.now();
 let totaltime = 0;
@@ -28,7 +32,7 @@ passwordBtn.addEventListener("click", async (e) => {
         }
     }
     if(!canBreak){
-        alert("A senha deve conter apenas letras minusculas");
+        alert("A senha contém caracteres não aceitos");
     }
     if(password && canBreak) {
         tempo1 = Date.now();
@@ -55,7 +59,7 @@ function step(password, tentativa, resolve) {
         return tentativa
     }
     setTimeout(() => {
-        for(let n  = 0; n < 1_000_000; n++) {
+        for(let n  = 0; n < 100_000; n++) {
             tentativa = increment(tentativa, alfabeto);
             if(tentativa === password){
                 
